b0cefd6793edea1a48f835b052da734643d7f815
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2013-04-10 16:44:41 +0200

    Sort directly in python, hopefully fixes #1 and #2.

--- SortNumerically.py
+++ SortNumerically.py
@@ -1,6 +1,9 @@
+# This simple plugin is based on:
+# http://www.codinghorror.com/blog/2007/12/sorting-for-humans-natural-sort-order.html
+
 import sublime
 import sublime_plugin
-import subprocess
+import re
 
 
 class SortNumericallyCommand(sublime_plugin.TextCommand):
@@ -10,12 +13,20 @@ class SortNumericallyCommand(sublime_plugin.TextCommand):
             # Selection is empty, use the entire buffer.
             regions = [sublime.Region(0, self.view.size())]
         for region in regions:
-            p = subprocess.Popen(['sort', '-n'], bufsize=-1,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                stdin=subprocess.PIPE)
-            output, error = p.communicate(self.view.substr(region).encode('utf-8'))
-            if error:
-                sublime.error_message(error.decode('utf-8'))
-            else:
-                self.view.replace(edit, region, output.decode('utf-8'))
+            lines = [self.view.substr(r) for r in self.view.lines(region)]
+            convert = lambda text: int(text) if text.isdigit() else text
+            alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+            sorted_lines = sorted(lines, key=alphanum_key)
+
+            # Determine the current line ending setting, so we can rejoin the
+            # sorted lines using the correct line ending character.
+            line_ending_character = '\n'  # Default.
+            line_endings = self.view.line_endings()
+            if line_endings == 'CR':
+                line_ending_character = '\r'
+            elif line_endings == 'Windows':
+                line_ending_character = '\r\n'
+
+            output = line_ending_character.join(sorted_lines)
+
+            self.view.replace(edit, region, output)
